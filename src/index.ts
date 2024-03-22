7979b69eb61bf7306766030755ac8c6516a1e1dc
João Vitor <devjohncb@gmail.com>
2024-03-22 16:52:24 -0300

    feat: add dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/server';
 
-server.listen(3333, () => {
-  console.log('server api listening');
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`server api listening on port ${process.env.PORT || 3333}`);
 });

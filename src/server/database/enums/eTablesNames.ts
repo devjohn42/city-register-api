25d1978da17a31a3b61e1dd64226e7401da5e257
João Vitor <devjohncb@gmail.com>
2024-03-27 00:30:17 -0300

    feat: add person model and migration

--- src/server/database/enums/eTablesNames.ts
+++ src/server/database/enums/eTablesNames.ts
@@ -1,3 +1,4 @@
 export enum TableNames {
   city = 'city',
+  person = 'person',
 }
---
+++ src/server/database/migrations/0001_create_person.ts
@@ -0,0 +1,28 @@
+import { Knex } from 'knex';
+import { TableNames } from '../enums/eTablesNames';
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(TableNames.person, (table) => {
+      table.bigIncrements('id').primary().index();
+      table.string('name').index().notNullable();
+      table.string('email').index().notNullable();
+      table
+        .bigInteger('cityId')
+        .index()
+        .notNullable()
+        .references('id')
+        .inTable(TableNames.city)
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    })
+    .then(() => {
+      console.log(`#Create table ${TableNames.person}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(TableNames.person).then(() => {
+    console.log(`# Create table ${TableNames.person}`);
+  });
+}
